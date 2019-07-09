f2005e534b18bb2fa79eb69bd1ada4734c1c8916
Prabal\Pranto <roni2067@gmail.com>
2019-07-09 14:51:26 +0600

    CIRMS

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,10 +3,6 @@ import 'package:crime_report/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 class CallApi{
-    // final String _url = 'http://192.168.1.3:8000/api/';
-    // final String _url1 = 'http://192.168.1.3:8000/uploads/';
-    // final String _url = 'http://api.cirms.xyz/api/';
-    // final String _url1 = 'http://api.cirms.xyz/public/uploads/';
     final String _url = 'http://crimeapi.letsruncode.com/api/';
     final String _url1 = 'http://crimeapi.letsruncode.com/public/uploads/';
 
